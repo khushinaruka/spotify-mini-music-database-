a4fe936398e2cb81c49a0712bc051e7d90851285
khushinaruka <narukakhushi082005@gmail.com>
2025-09-06 16:15:22 +0530

    Create schema .sql

---
+++ schema .sql
@@ -0,0 +1,33 @@
+CREATE TABLE Artists (
+    artist_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    country VARCHAR(50),
+    total_followers INT
+);
+
+CREATE TABLE Albums (
+    album_id INT PRIMARY KEY,
+    album_name VARCHAR(100) NOT NULL,
+    release_year INT,
+    artist_id INT,
+    FOREIGN KEY (artist_id) REFERENCES Artists(artist_id)
+);
+
+CREATE TABLE Songs (
+    song_id INT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    duration INT, -- in seconds
+    genre VARCHAR(50),
+    play_count INT DEFAULT 0,
+    artist_id INT,
+    album_id INT,
+    FOREIGN KEY (artist_id) REFERENCES Artists(artist_id),
+    FOREIGN KEY (album_id) REFERENCES Albums(album_id)
+);
+
+CREATE TABLE Playlists (
+    playlist_id INT PRIMARY KEY,
+    playlist_name VARCHAR(100) NOT NULL,
+    user_id INT,
+    created_date DATE
+);
