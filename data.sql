9195a37a56231d93a4363cf30e1d9875038ddf8a
khushinaruka <narukakhushi082005@gmail.com>
2025-09-06 16:17:16 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,52 @@
+-- Artists (10 rows)
+INSERT INTO Artists VALUES (1, 'Arijit Singh', 'India', 5000000);
+INSERT INTO Artists VALUES (2, 'Taylor Swift', 'USA', 8000000);
+INSERT INTO Artists VALUES (3, 'Ed Sheeran', 'UK', 7500000);
+INSERT INTO Artists VALUES (4, 'Shreya Ghoshal', 'India', 4000000);
+INSERT INTO Artists VALUES (5, 'The Weeknd', 'Canada', 9000000);
+INSERT INTO Artists VALUES (6, 'Badshah', 'India', 3000000);
+INSERT INTO Artists VALUES (7, 'Adele', 'UK', 8500000);
+INSERT INTO Artists VALUES (8, 'Diljit Dosanjh', 'India', 2500000);
+INSERT INTO Artists VALUES (9, 'Billie Eilish', 'USA', 7200000);
+INSERT INTO Artists VALUES (10, 'Sonu Nigam', 'India', 3500000);
+
+-- Albums (10 rows)
+INSERT INTO Albums VALUES (1, 'Love Songs', 2020, 1);
+INSERT INTO Albums VALUES (2, '1989', 2014, 2);
+INSERT INTO Albums VALUES (3, 'Divide', 2017, 3);
+INSERT INTO Albums VALUES (4, 'Melodies', 2021, 4);
+INSERT INTO Albums VALUES (5, 'After Hours', 2020, 5);
+INSERT INTO Albums VALUES (6, 'Rap Star', 2019, 6);
+INSERT INTO Albums VALUES (7, '25', 2015, 7);
+INSERT INTO Albums VALUES (8, 'Punjabi Beats', 2021, 8);
+INSERT INTO Albums VALUES (9, 'Happier Than Ever', 2021, 9);
+INSERT INTO Albums VALUES (10, 'Classic Hits', 2010, 10);
+
+-- Songs (15 rows)
+INSERT INTO Songs VALUES (1, 'Tum Hi Ho', 250, 'Romantic', 1000000, 1, 1);
+INSERT INTO Songs VALUES (2, 'Channa Mereya', 300, 'Romantic', 800000, 1, 1);
+INSERT INTO Songs VALUES (3, 'Blank Space', 220, 'Pop', 2000000, 2, 2);
+INSERT INTO Songs VALUES (4, 'Style', 240, 'Pop', 1500000, 2, 2);
+INSERT INTO Songs VALUES (5, 'Shape of You', 240, 'Pop', 5000000, 3, 3);
+INSERT INTO Songs VALUES (6, 'Perfect', 260, 'Romantic', 4200000, 3, 3);
+INSERT INTO Songs VALUES (7, 'Sun Raha Hai', 280, 'Sad', 600000, 4, 4);
+INSERT INTO Songs VALUES (8, 'Teri Meri', 270, 'Classical', 300000, 4, 4);
+INSERT INTO Songs VALUES (9, 'Blinding Lights', 200, 'Pop', 6000000, 5, 5);
+INSERT INTO Songs VALUES (10, 'Rap Star', 210, 'Hip Hop', 1200000, 6, 6);
+INSERT INTO Songs VALUES (11, 'Hello', 295, 'Soul', 5500000, 7, 7);
+INSERT INTO Songs VALUES (12, 'Someone Like You', 275, 'Soul', 4800000, 7, 7);
+INSERT INTO Songs VALUES (13, 'G.O.A.T', 250, 'Punjabi', 900000, 8, 8);
+INSERT INTO Songs VALUES (14, 'Bad Guy', 194, 'Pop', 7000000, 9, 9);
+INSERT INTO Songs VALUES (15, 'Kal Ho Naa Ho', 310, 'Bollywood', 3500000, 10, 10);
+
+-- Playlists (10 rows)
+INSERT INTO Playlists VALUES (1, 'Chill Vibes', 101, '2025-09-01');
+INSERT INTO Playlists VALUES (2, 'Romantic Hits', 102, '2025-09-02');
+INSERT INTO Playlists VALUES (3, 'Pop Party', 103, '2025-09-03');
+INSERT INTO Playlists VALUES (4, 'Soulful', 104, '2025-09-04');
+INSERT INTO Playlists VALUES (5, 'Bollywood Mix', 105, '2025-09-05');
+INSERT INTO Playlists VALUES (6, 'Top 10 Global', 106, '2025-09-06');
+INSERT INTO Playlists VALUES (7, 'Punjabi Tadka', 107, '2025-09-07');
+INSERT INTO Playlists VALUES (8, 'Hip Hop Hits', 108, '2025-09-08');
+INSERT INTO Playlists VALUES (9, 'Evening Relax', 109, '2025-09-09');
+INSERT INTO Playlists VALUES (10, 'Morning Motivation', 110, '2025-09-10');
